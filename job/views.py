90fb26000988a7dc3ab7dc5a57ff7ce5d5d30b54
bellarej <bellarej.tarek@gmail.com>
2020-09-15 19:30:04 +0100

    Link and menu

--- job/views.py
+++ job/views.py
@@ -3,6 +3,7 @@ from .models import Job
 from django.core.paginator import Paginator
 from .form import ApplyForm, JobForm
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -33,6 +34,7 @@ def job_detail(request, slug):
     context ={'job': job_detail, 'form1':form}
     return render(request, 'job/job_detail.html', context)
 
+@login_required
 def add_job(request):
 
     if request.method == 'POST':
